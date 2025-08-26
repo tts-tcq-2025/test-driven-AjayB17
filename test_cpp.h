7f64dcfaa5781529b31016f12e6401424fa494a3
AjayB17 <abajstyles@gmail.com>
2025-08-26 13:59:25 +0530

    Update test_cpp.h

--- test_cpp.h
+++ test_cpp.h
@@ -1,16 +1,1 @@
-#include <cassert>
-#include <iostream>
-#include <string>
-#include <vector>
-#include <stdexcept>
-#include <sstream>
 
-void testSingleNumber();
-void testTwoNumbers();
-void testUnknownAmountOfNumbers();
-void testNewlinesAsDelimiters();
-void testCustomSingleCharDelimiter();
-void testCustomMultiCharDelimiter();
-void testMultipleCustomDelimiters();
-void testNegativeNumbersThrow();
-void testNumbersOver1000Ignored();
